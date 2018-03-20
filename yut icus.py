f083a7920ca1fe2c109164677c1375c3d59bc504
Big Man <37562221+BigMan24@users.noreply.github.com>
2018-03-20 12:44:33 +1100

    Update yut icus.py
    Test

--- yut icus.py
+++ yut icus.py
@@ -1,1 +1,8 @@
 from fixerio import Fixerio
+import pandas
+import json
+currency1 = int(input("What do you want to convert from? "))
+currency2 = input("What do you want to convert to? ")
+fxrio = Fixerio(base=currency1)
+fxrio.latest(symbols=[currency1, currency2])
+print("Currency amount here for the first currency you inputted " + currency1 + " is worth " + " Currency 2 amount here " + currency2 )
