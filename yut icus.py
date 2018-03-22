da289cdb649d878b2f4cbd17b4abf79ff4c1ce2c
KoppXO <37562188+KoppXO@users.noreply.github.com>
2018-03-22 15:41:32 +1100

    Update yut icus.py

--- yut icus.py
+++ yut icus.py
@@ -1,3 +1,4 @@
+import sys
 from forex_python.converter import CurrencyRates
 c = CurrencyRates()
 from forex_python.converter import CurrencyCodes
@@ -7,9 +8,21 @@ print("Welcome to Floyd and Jeremy's money converter!"'''
 print('==============================================''''
 ''')
 currency1 = input('What would you like to convert? ')
+if len(currency1) !=3:
+  print('Sorry this is not a valid number:Exiting Program...')
+  sys.exit('')
 currency2 = input('What would you like to convert it to? ')
+if len(currency2) !=3:
+  print('Sorry this is not a valid number:EXITING PROGRAM...')
+  sys.exit('')
 currency3 = int(input('How much would you like to convert'))
-print(c.convert(currency1, currency2, currency3),n.get_symbol(currency2))
+if len(currency3) !=3:
+  print('Sorry this is not a valid number:EXITING PROGRAM...')
+  sys.exit('')
+currency5 = currency2.upper()
+currency4 = currency1.upper()
+
+print(n.get_symbol(currency5),c.convert(currency4, currency5, currency3))
 
 
 
