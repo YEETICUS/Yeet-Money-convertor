2c3b186a635748160f163fdf32c84b14028c7811
KoppXO <37562188+KoppXO@users.noreply.github.com>
2018-03-22 12:02:27 +1100

    Update yut icus.py

--- yut icus.py
+++ yut icus.py
@@ -1,10 +1,6 @@
-from fixerio import Fixerio
-import pandas
-import json
-currency1 = input("What do you want to convert from? ")
-currency2 = input("What do you want to convert to? ")
-fxrio = Fixerio(base= currency1)
-fxrio = Fixerio(symbols=[currency1, currency2])
-fxrio.latest(symbols=[currency1, currency2])
-print("Currency amount here for the first currency you inputted " + currency1 + " is worth " + " Currency 2 amount here " + currency2 )
-input(fxrio.latest())
+from forex_python.converter import CurrencyRates
+c = CurrencyRates()
+currency1 = input('What would you like to convert? ')
+currency2 = input('What would you like to convert it to? ')
+y = c.convert(currency1, currency2, 1)
+print(y)
